711202b50b5cb07b9dcf88d629c542c5cc925026
Dhany Nurdiansyah <dhanyn.dhan@gmail.com>
2018-07-05 11:16:57 +0700

    update documentation

--- bootsalert.js
+++ bootsalert.js
@@ -15,8 +15,8 @@ var bootsalert = function(val){
     {
         /**
          * className    : set bootstrap alert class, like success, info, ..., or developer can use their own custom class
-         * message      : the message will displayed to user
-         * container    : alert will be inserted to this component, this component must have id attributes
+         * message      : message that will displayed to user
+         * container    : alert will be inserted to this component. Selected component must have id attributes
          * closebtn     : if true, alert will have close button with fade effect
          */
         className   = val.className;
